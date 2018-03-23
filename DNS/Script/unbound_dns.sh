d502e7a58835655dc19189ab0ce90ccafd944307
Raxgahrax <jujumercier1991@yahoo.fr>
2018-03-23 11:00:37 +0100

    M-à-J

--- DNS/Script/unbound_dns.sh
+++ DNS/Script/unbound_dns.sh
@@ -8,19 +8,28 @@
 sudo ntpdate 0.ch.pool.ntp.org
 #Arrêt d'Unbound
 sudo service unbound stop
+#Suppression du fichier actuel
+sudo chown pi:pi /var/lib/unbound/ads.conf
+rm /var/lib/unbound/ads.conf
 #Téléchargement des sources
 wget -P /home/pi/Desktop/DNS/temp http://someonewhocares.org/hosts/zero/hosts
 mv /home/pi/Desktop/DNS/temp/hosts /home/pi/Desktop/DNS/temp/hosts.txt
-python /home/pi/Desktop/DNS/generate-domains-blacklist.py > /home/pi/Desktop/DNS/temp/ads.conf
+sleep 1
+wget -P /home/pi/Desktop/DNS/temp/ https://easylist-downloads.adblockplus.org/malwaredomains_full.txt
+sleep 1
+wget -P /home/pi/Desktop/DNS/temp/ https://raw.githubusercontent.com/HexxiumCreations/threat-list/gh-pages/hexxiumthreatlist.txt
+#Suppression des lignes non-essentielles (commentaires, etc.)
 sleep 5
+sed -i '/^[[!]/d; s/^||//; s/\^$//' /home/pi/Desktop/DNS/temp/malwaredomains_full.txt
+sleep 5
+sed -i '/^[[!]/d; s/^||//; s/\^$//' /home/pi/Desktop/DNS/temp/hexxiumthreatlist.txt
+sleep 5
+#Génération du listing
+python /home/pi/Desktop/DNS/generate-domains-blacklist.py > /home/pi/Desktop/DNS/temp/ads.conf
 #Suppression des lignes non-essentielles (commentaires, etc.)
 sed -i '/^$/d;/#/d;s/www\.//' -i /home/pi/Desktop/DNS/temp/ads.conf
-sleep 5
 #Suppression des doublons
 sort -u /home/pi/Desktop/DNS/temp/ads.conf > /home/pi/Desktop/DNS/temp/ads_temp.conf
-#Suppression de l'ancien fichier ads.conf
-sudo chown pi:pi /var/lib/unbound/ads.conf
-rm /var/lib/unbound/ads.conf
 sleep 5
 #Mise en page du futur ads.conf
 for a in `cat /home/pi/Desktop/DNS/temp/ads_temp.conf | dos2unix`; do
