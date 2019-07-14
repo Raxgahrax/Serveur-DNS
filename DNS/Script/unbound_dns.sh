1949c0fbcce21a63d18f65254c1e8dc80cb21d7e
Raxgahrax <jujumercier1991@yahoo.fr>
2019-07-14 21:02:08 +0200

    M-à-J

--- DNS/Script/unbound_dns.sh
+++ DNS/Script/unbound_dns.sh
@@ -10,35 +10,39 @@ sudo ntpdate 0.ch.pool.ntp.org
 #Arrêt d'Unbound
 sudo service unbound stop
 #Suppression du fichier actuel
-sudo chown pi:pi /var/lib/unbound/ads.conf
+sudo chown odroid:odroid /var/lib/unbound/ads.conf
 rm /var/lib/unbound/ads.conf
 #Téléchargement des sources
-wget -P /home/pi/Desktop/DNS/temp http://someonewhocares.org/hosts/zero/hosts
-mv /home/pi/Desktop/DNS/temp/hosts /home/pi/Desktop/DNS/temp/hosts.txt
+wget -P /home/odroid/Bureau/DNS/temp http://someonewhocares.org/hosts/zero/hosts
+mv /home/odroid/Bureau/DNS/temp/hosts /home/odroid/Bureau/DNS/temp/hosts.txt
 sleep 1
-wget -P /home/pi/Desktop/DNS/temp http://1hos.cf
+wget -P /home/odroid/Bureau/DNS/temp http://1hos.cf
+sleep 1
+wget -P /home/odroid/Bureau/DNS/temp http://rlwpx.free.fr/WPFF/htrc.7z
+7z x /home/odroid/Bureau/DNS/temp/htrc.7z
+mv /home/odroid/Hosts.trc /home/odroid/Bureau/DNS/temp/Hosts.trc
 #https://t.me/badmojr
 #wget -P /home/pi/Desktop/DNS/temp http://1hosts.cf
-mv /home/pi/Desktop/DNS/temp/index.html /home/pi/Desktop/DNS/temp/OneHosts.txt
+mv /home/odroid/Bureau/DNS/temp/index.html /home/odroid/Bureau/DNS/temp/OneHosts.txt
 sleep 1
-wget -P /home/pi/Desktop/DNS/temp/ https://easylist-downloads.adblockplus.org/malwaredomains_full.txt
+wget -P /home/odroid/Bureau/DNS/temp/ https://easylist-downloads.adblockplus.org/malwaredomains_full.txt
 sleep 1
-wget -P /home/pi/Desktop/DNS/temp/ https://raw.githubusercontent.com/HexxiumCreations/threat-list/gh-pages/hexxiumthreatlist.txt
+wget -P /home/odroid/Bureau/DNS/temp/ https://raw.githubusercontent.com/HexxiumCreations/threat-list/gh-pages/hexxiumthreatlist.txt
 #Suppression des lignes non-essentielles (commentaires, etc.)
 sleep 5
-sed -i '/^[[!]/d; s/^||//; s/\^$//' /home/pi/Desktop/DNS/temp/malwaredomains_full.txt
+sed -i '/^[[!]/d; s/^||//; s/\^$//' /home/odroid/Bureau/DNS/temp/malwaredomains_full.txt
 sleep 5
-sed -i '/^[[!]/d; s/^||//; s/\^$//' /home/pi/Desktop/DNS/temp/hexxiumthreatlist.txt
+sed -i '/^[[!]/d; s/^||//; s/\^$//' /home/odroid/Bureau/DNS/temp/hexxiumthreatlist.txt
 sleep 5
 #Génération du listing
-python /home/pi/Desktop/DNS/generate-domains-blacklist.py > /home/pi/Desktop/DNS/temp/ads.conf
+python /home/odroid/Bureau/DNS/generate-domains-blacklist.py > /home/odroid/Bureau/DNS/temp/ads.conf
 #Suppression des lignes non-essentielles (commentaires, etc.)
-sed -i '/^$/d;/#/d;s/www\.//' -i /home/pi/Desktop/DNS/temp/ads.conf
+sed -i '/^$/d;/#/d;s/www\.//' -i /home/odroid/Bureau/DNS/temp/ads.conf
 #Suppression des doublons
-sort -u /home/pi/Desktop/DNS/temp/ads.conf > /home/pi/Desktop/DNS/temp/ads_temp.conf
+sort -u /home/odroid/Bureau/DNS/temp/ads.conf > /home/odroid/Bureau/DNS/temp/ads_temp.conf
 sleep 5
 #Mise en page du futur ads.conf
-for a in `cat /home/pi/Desktop/DNS/temp/ads_temp.conf | dos2unix`; do
+for a in `cat /home/odroid/Bureau/DNS/temp/ads_temp.conf | dos2unix`; do
 echo 'local-zone: "'$a'" redirect'
 echo 'local-data: "'$a' A 0.0.0.0"'
 done >> /var/lib/unbound/ads.conf
@@ -47,9 +51,9 @@ sleep 5
 sudo chown root:root /var/lib/unbound/ads.conf
 sudo chmod 644 /var/lib/unbound/ads.conf
 #Suppression des fichiers temporaires
-rm -rf /home/pi/Desktop/DNS/temp/*
+rm -rf /home/odroid/Bureau/DNS/temp/*
 #Redémarrage d'Unbound
 sudo service unbound restart
 #Rapport sur log et fin du script
-echo "Rapport de lancement du $(date)" >> /home/pi/Desktop/DNS/Logs/Process.log
+echo "Rapport de lancement du $(date)" >> /home/odroid/Bureau/DNS/Logs/Process.log
 exit
